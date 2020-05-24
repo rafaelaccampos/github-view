a6820da24a3371e2f87986c4f2d2307d90cd8b73
rafaelaccampos <rafaela-cassiano@hotmail.com>
2020-05-24 17:26:27 -0300

    feat: Connect the application with github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,53 +1,63 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 const Dashboard: React.FunctionComponent = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [newRepository, setNewRepository] = useState('');
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepository}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+
+    setNewRepository('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="GitHub Explorer" />
       <Title>Explore repositórios no GitHub</Title>
-      <Form action="">
-        <input placeholder="Nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepository}
+          onChange={(e) => setNewRepository(e.target.value)}
+          placeholder="Nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/22009981?s=460&u=b1416b4d67af05ac9f070ec42682a40971b25c76&v=4"
-            alt="Rafaela Campos"
-          />
-          <div>
-            <strong>Rocketfy</strong>
-            <p>Interface recriada do Pipefy seguindo o vídeo da Rocketseat</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/22009981?s=460&u=b1416b4d67af05ac9f070ec42682a40971b25c76&v=4"
-            alt="Rafaela Campos"
-          />
-          <div>
-            <strong>Rocketfy</strong>
-            <p>Interface recriada do Pipefy seguindo o vídeo da Rocketseat</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/22009981?s=460&u=b1416b4d67af05ac9f070ec42682a40971b25c76&v=4"
-            alt="Rafaela Campos"
-          />
-          <div>
-            <strong>Rocketfy</strong>
-            <p>Interface recriada do Pipefy seguindo o vídeo da Rocketseat</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
