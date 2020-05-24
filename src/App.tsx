40da247023201ddf247c6848d967ac691425b1e8
rafaelaccampos <rafaela-cassiano@hotmail.com>
2020-05-23 21:23:37 -0300

    Add styled components and creating application global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-
-import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+
 const App: React.FunctionComponent = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
