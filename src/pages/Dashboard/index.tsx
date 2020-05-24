5499db9ecdbf162ded6a55fb53cf6b0ca273f023
rafaelaccampos <rafaela-cassiano@hotmail.com>
2020-05-24 16:00:55 -0300

    Add style (css, animations) to dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FunctionComponent = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+      <Form action="">
+        <input placeholder="Nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/22009981?s=460&u=b1416b4d67af05ac9f070ec42682a40971b25c76&v=4"
+            alt="Rafaela Campos"
+          />
+          <div>
+            <strong>Rocketfy</strong>
+            <p>Interface recriada do Pipefy seguindo o vídeo da Rocketseat</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/22009981?s=460&u=b1416b4d67af05ac9f070ec42682a40971b25c76&v=4"
+            alt="Rafaela Campos"
+          />
+          <div>
+            <strong>Rocketfy</strong>
+            <p>Interface recriada do Pipefy seguindo o vídeo da Rocketseat</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/22009981?s=460&u=b1416b4d67af05ac9f070ec42682a40971b25c76&v=4"
+            alt="Rafaela Campos"
+          />
+          <div>
+            <strong>Rocketfy</strong>
+            <p>Interface recriada do Pipefy seguindo o vídeo da Rocketseat</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
